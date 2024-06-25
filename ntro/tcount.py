cf81438de2de6ea6d54cec010e2a9a894756bac4
Mathias Weiden <mtweiden@berkeley.edu>
2024-06-25 12:34:44 -0700

    Added documentation

--- ntro/tcount.py
+++ ntro/tcount.py
@@ -1,36 +1,59 @@
+from bqskit.ir.circuit import Circuit
 from bqskit.ir.opt.cost.generator import CostFunctionGenerator
 from bqskit.ir.opt.cost.differentiable import DifferentiableCostFunction
+from bqskit.ir.opt.cost import CostFunction
+from bqskit.qis.unitary import RealVector
+from bqskit.qis.state.state import StateVector
+from bqskit.qis.unitary.unitarymatrix import UnitaryMatrix
+
 import numpy as np
+import numpy.typing as npt
+
 
 class RelaxedTCountCostGenerator(CostFunctionGenerator):
-    def __init__(self, period=4):
+    def __init__(self, period: int=4) -> None:
+        """
+        Constructor for RelaxedTCountCostGenerator.
+
+        Args:
+            period (int): The period of the triangle wave cost function.
+                Parameter values will be pushed towards multiple of 
+                pi / period. (Default: 4)
+        """
         super().__init__()
         self.period = period
 
-    def gen_cost(self, circuit, target):
+    def gen_cost(
+        self,
+        circuit: Circuit,
+        target: UnitaryMatrix | StateVector,
+    ) -> CostFunction:
         return RelaxedTCount(self.period)
 
 
-
 class RelaxedTCount(DifferentiableCostFunction):
-    def __init__(self, period):
+    def __init__(self, period: int) -> None:
         super().__init__()
         self.period = period
 
-    def get_cost(self, params):
+    def get_cost(self, params: RealVector) -> float:
         period = self.period
         if params.shape[0] < 1:
             return 0
-        return (2/period) * np.sum(np.abs(np.mod(params-period/2, period) - period/2))
+        deviation = self.get_arr(params)
+        cost = np.sum(deviation)
+        return cost
 
-    def get_arr(self, params):
+    def get_arr(self, params: RealVector) -> npt.NDArray[np.float64]:
         period = self.period
         if params.shape[0] < 1:
             return []
-        return (2/period)*np.abs(np.mod(params-period/2, period) - period/2)
+        shifted_params = np.mod(params - period / 2, period)
+        deviation = np.abs(shifted_params - period / 2)
+        return (2 / period) * deviation
 
-    def get_grad(self, params):
+    def get_grad(self, params: RealVector) -> npt.NDArray[np.float64]:
         period = self.period
-        return -(2/period) * np.sign(np.mod(params, period) - period/2)
-        
-
+        deviation = np.mod(params, period) - period / 2
+        signs = np.sign(deviation)
+        return -1 * (2 / period) * signs
