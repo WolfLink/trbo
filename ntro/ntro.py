b84ec9bfa9e7b9abf99ed534c3fc9ffa938c8404
Mathias Weiden <mtweiden@berkeley.edu>
2024-07-11 12:59:47 -0400

    Clean up

--- ntro/ntro.py
+++ ntro/ntro.py
@@ -20,6 +20,7 @@ from ntro.clift import rz_gates
 
 from ntro.two_pass_minimization import TwoPassMinimization
 from ntro.tcount import RelaxedTCountCostGenerator
+from ntro.tcount import get_arr
 from ntro.rz_to_t import RzToTPass, RzToT_ScanningBruteForcePass
 
 import logging
@@ -30,7 +31,6 @@ _logger = logging.getLogger(__name__)
 class NumericalTReductionPass(BasePass):
     def __init__(
         self,
-        utry: Optional[UnitaryLike] = None,
         success_threshold: float = 1e-8,
         **kwargs,
     ) -> None:
@@ -38,10 +38,6 @@ class NumericalTReductionPass(BasePass):
         Construct a NumericalTReductionPass
 
         Args:
-            utry (Optional[UnitaryLike]): The target unitary to approximate.
-                If None, the target unitary is taken from the input circuit.
-                (Default: None)
-            
             success_threshold (float): The synthesis success threshold.
                 (Default: 1e-8)
         """
@@ -50,10 +46,10 @@ class NumericalTReductionPass(BasePass):
             'method' : TwoPassMinimization(),
             'kwargs' : {'pifrac' : 4}
         }
-        self.utry = utry
         self.success_threshold = success_threshold
         self.extra_kwargs = kwargs
         self.acceptable_gates = clifford_gates + t_gates + rz_gates
+        self.target_periods = [0.5, 0.25]
 
     async def run(self, circuit: Circuit, data: PassData = {}) -> None:
         # Check that circuit has been converrted to Clifford+T+Rz
@@ -64,18 +60,26 @@ class NumericalTReductionPass(BasePass):
             )
             raise ValueError(m)
 
-        best_circuit = circuit
-        utry = self.utry
-        if utry is None:
-            utry = circuit.get_unitary()
-        best_circuit.unfold_all()
-
-        # run 1st pass minimization
+        utry = circuit.get_unitary()
         best_result = circuit
+        best_result.unfold_all()
+
+        # trial_circuit will try to round as many parameters as possible to
+        # nice values
         trial_circuit = circuit.copy()
-        for period in [0.5, 0.25]:
-            angle = period * np.pi
-            for _ in range(circuit.num_params+1):
+
+        for period in self.target_periods:
+
+            period = period * np.pi
+
+            self.instantiate_options["method"] = TwoPassMinimization(
+                pass_2_cost_gen=RelaxedTCountCostGenerator(period=period),
+                success_threshold=self.success_threshold,
+                **self.extra_kwargs,
+            )
+
+            for _ in range(circuit.num_params + 1):
+
                 # check for constant circuits
                 if trial_circuit.num_params < 1:
                     dist = trial_circuit.get_unitary().get_distance_from(utry)
@@ -85,21 +89,19 @@ class NumericalTReductionPass(BasePass):
                         trial_circuit = best_result.copy()
                     break
 
-                self.instantiate_options["method"] = TwoPassMinimization(
-                    pass_2_cost_gen=RelaxedTCountCostGenerator(period=angle),
-                    success_threshold=self.success_threshold,
-                    **self.extra_kwargs,
-                )
-                relaxedTCount = RelaxedTCountCostGenerator(angle).gen_cost(best_result, utry)
-                result = await get_runtime().submit(
+                print(trial_circuit.gate_counts)
+
+                result: Circuit | None = await get_runtime().submit(
                         self.instantiate_options["method"].multi_start_instantiate_async,
                         trial_circuit,
                         target=utry,
                         num_starts=32,
                         # **self.instantiate_options
                 )
+
                 if result is None:
                     break
+
                 cost_calc = HilbertSchmidtCostGenerator().gen_cost(result, utry)
                 # verify that the new result passes the threshold
                 if cost_calc(result.params) < self.success_threshold:
@@ -110,13 +112,13 @@ class NumericalTReductionPass(BasePass):
 
                 trial_circuit: Circuit = best_result.copy()
 
-                param_scores = relaxedTCount.get_arr(trial_circuit.params)
+                param_scores = get_arr(trial_circuit.params, period)
                 minval = np.min(param_scores)
                 did_round = False
                 for cycle, op in trial_circuit.operations_with_cycles():
                     if len(op.params) != 1:
                         continue
-                    if relaxedTCount.get_arr(np.array(op.params))[0] == minval:
+                    if get_arr(np.array(op.params), period)[0] == minval:
                         rounded = circuit_for_rounded_val(op.params[0], period)
                         trial_circuit.replace_gate(
                             (cycle, op.location[0]), rounded, op.location
