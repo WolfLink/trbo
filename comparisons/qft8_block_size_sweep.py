aeb972f4531f332b1a14a417962878d324e0b354
Marc Davis <mgd@mit.edu>
2025-04-27 23:08:50 -0400

    fixed doing 5 twice instead of doing 4

--- comparisons/qft8_block_size_sweep.py
+++ comparisons/qft8_block_size_sweep.py
@@ -3,4 +3,4 @@ from run_experiment import run_benchmarks
 directory = "./quipper_circuits/optimizer/QFT_and_Adders/"
 files = [directory + "QFT8_before", directory + "QFT8_after"]
 
-run_benchmarks(files=files, thresholds=[1e-5], block_sizes=[3,5,5,6,7,8], path="./qft8_block_size_sweep")
+run_benchmarks(files=files, thresholds=[1e-5], block_sizes=[3,4,5,6,7,8], path="./qft8_block_size_sweep")
