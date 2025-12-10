cec7f95a465ce9aa530ec82814eb3b7d09613bcb
Marc Davis <mgd@mit.edu>
2025-12-09 22:08:02 -0500

    forgot to include discretization.py in the last commit

---
+++ ntro/discretization.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from bqskit.ir.opt.cost.generator import CostFunctionGenerator
+
+class RzDiscretization():
+    def nearest_gate(self, rz_angle: float) -> CircuitGate:
+        raise NotImplementedError
+
+    def param_distances(self, params: [float]) -> [float]:
+        raise NotImplementedError
+
+    def cost_generator(self, N: int) -> CostFunctionGenerator:
+        raise NotImplementedError
+
+    def residuals_generator(self, N: int, dim: int, blacklist: Optional[[int]] = None) -> CostFunctionGenerator:
+        raise NotImplementedError
+
+    def success_comparator(self, a, b) -> bool:
+        raise NotImplementedError
